750be886f458d972670dd3b9e37ab03230e29840
Suresh Shrestha <sureshcstha@gmail.com>
2025-03-06 09:17:11 -0700

    input Sanitization using sanitize-html

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+const sanitizeHtml = require('sanitize-html');
+
+// Function to sanitize inputs
+const sanitizeInput = (input) => sanitizeHtml(input, { allowedTags: [], allowedAttributes: {} });
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true, maxlength: 50 },
-    lastName: { type: String, maxlength: 50 },
+    firstName: { type: String, required: true, maxlength: 50, set: sanitizeInput },
+    lastName: { type: String, maxlength: 50, set: sanitizeInput },
     birthdate: { type: Date, required: true },
-    email: { type: String, required: true, unique: true, maxlength: 100 },
-    phone: { type: String, required: true, unique: true, maxlength: 15 },
+    email: { type: String, required: true, unique: true, maxlength: 100, set: sanitizeInput },
+    phone: { type: String, required: true, unique: true, maxlength: 15, set: sanitizeInput },
     subscribed: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now() },
 });
